6a6d5156b070d267415a2e1f7a5c90b24f945ef8
Steve Losh <steve@stevelosh.com>
2011-03-09 19:35:17 -0500

    Update the publish script to add a pull first.

--- site/publish.sh
+++ site/publish.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+hg -R ~/src/sjl.bitbucket.org pull -u
 rsync --delete -az . ~/src/sjl.bitbucket.org/gundo.vim
 hg -R ~/src/sjl.bitbucket.org commit -Am 'gundo.vim: Update documentation.'
 hg -R ~/src/sjl.bitbucket.org push
