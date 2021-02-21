c5efef192b975b8e7d5fa3c6db932648d3b76323
Steve Losh <steve@stevelosh.com>
2021-02-21 12:18:28 -0500

    Fix doc script

--- site/publish.sh
+++ site/publish.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 hg -R ~/src/docs.stevelosh.com pull -u
-rsync --delete -az . ~/src/docs.stevelosh.com /gundo.vim
+rsync --delete -az . ~/src/docs.stevelosh.com/gundo.vim
 hg -R ~/src/docs.stevelosh.com commit -Am 'gundo.vim: Update documentation.'
 hg -R ~/src/docs.stevelosh.com push
